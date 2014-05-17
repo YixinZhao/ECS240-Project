7d3a6b917e20ebe5b79eb9f396664f23a98523d4
Yixin Zhao <zyxivy@gmail.com>
2014-05-17 15:29:15 -0700

    modified:   SourceCode/bin/ecs240/Activator.class 	modified:   SourceCode/bin/ecs240/views/TopologyEditorView$1.class 	modified:   SourceCode/bin/ecs240/views/TopologyEditorView$2.class 	modified:   SourceCode/bin/ecs240/views/TopologyEditorView$3.class 	modified:   SourceCode/bin/ecs240/views/TopologyEditorView.class 	modified:   SourceCode/plugin.xml 	modified:   SourceCode/src/ecs240/Activator.java 	modified:   SourceCode/src/ecs240/views/TopologyEditorView.java

--- SourceCode/src/ecs240/Activator.java
+++ SourceCode/src/ecs240/Activator.java
@@ -1,8 +1,15 @@
 package ecs240;
 
+import java.util.MissingResourceException;
+
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+
+import ecs240.views.TopologyEditorView;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -14,7 +21,7 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -23,16 +30,30 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+
+		ImageRegistry ir = plugin.getImageRegistry();
+		ir.put(TopologyEditorView.SWITCH,
+				Activator.getImageDescriptor(TopologyEditorView.switchIcon));
+		ir.put(TopologyEditorView.SERVER,
+				Activator.getImageDescriptor(TopologyEditorView.serverIcon));
+		ir.put(TopologyEditorView.CLIENT,
+				Activator.getImageDescriptor(TopologyEditorView.clientIcon));
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -41,7 +62,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
@@ -49,13 +70,42 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+	public static Image getImage(String name) {
+
+		ImageRegistry ir = plugin.getImageRegistry();
+		try {
+			Image img = plugin.getImageRegistry().get(name);
+			if (img == null) {
+				ImageDescriptor desc;
+				if (name.equalsIgnoreCase(TopologyEditorView.SWITCH)) {
+					desc = Activator
+							.getImageDescriptor(TopologyEditorView.switchIcon);
+				} else if (name.equalsIgnoreCase(TopologyEditorView.SERVER)) {
+					desc = Activator
+							.getImageDescriptor(TopologyEditorView.serverIcon);
+				} else if (name.equalsIgnoreCase(TopologyEditorView.CLIENT)) {
+					desc = Activator
+							.getImageDescriptor(TopologyEditorView.clientIcon);
+				} else {
+					return null;
+				}
+				ir.put(name, desc);
+				img = ir.get(name);
+			}
+			return img;
+		} catch (Exception e) {
+		}
+		return null;
 	}
 }
--- SourceCode/src/ecs240/views/TopologyEditorView.java
+++ SourceCode/src/ecs240/views/TopologyEditorView.java
@@ -24,6 +24,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
@@ -31,10 +32,13 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.swt.widgets.Widget;
 
 import org.eclipse.swt.dnd.*;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
+
+import ecs240.Activator;
 
 public class TopologyEditorView extends ViewPart {
 
@@ -42,6 +46,14 @@ public class TopologyEditorView extends ViewPart {
 	 * The ID of the view as specified by the extension.
 	 */
 	public static final String ID = "ecs240.views.TopologyEditorView";
+
+	public static final String switchIcon = "icons/switch.jpg";
+	public static final String serverIcon = "icons/server.jpg";
+	public static final String clientIcon = "icons/client.jpg";
+
+	public static final String SWITCH = "switch";
+	public static final String SERVER = "server";
+	public static final String CLIENT = "client";
 
 	private Display display;
 	private FormToolkit toolkit;
@@ -58,10 +70,10 @@ public class TopologyEditorView extends ViewPart {
 	private DragSource source;
 	private DropTarget target;
 	private TextTransfer textTransfer;
-	private FileTransfer fileTransfer;
 
 	private Label anchorLabel;
-	private Point mousePt;
+
+	private Widget widget;
 
 	/**
 	 * The constructor.
@@ -90,12 +102,11 @@ public class TopologyEditorView extends ViewPart {
 		sourceSection.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false,
 				true));
 		sourceSection.setText("Network Component");
-
 		sourceArea = toolkit.createComposite(sourceSection, SWT.BORDER);
 		sourceArea.setLayout(new GridLayout());
 
 		dragList = new List(sourceArea, SWT.SINGLE | SWT.BORDER | SWT.V_SCROLL);
-		dragList.setItems(new String[] { "Switch", "Router", "Host" });
+		dragList.setItems(new String[] { SWITCH, SERVER, CLIENT });
 		dragList.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1,
 				1));
 		sourceSection.setClient(sourceArea);
@@ -109,57 +120,71 @@ public class TopologyEditorView extends ViewPart {
 
 		targetArea = toolkit.createComposite(targetSection, SWT.BORDER);
 		targetArea.setLayout(new FormLayout());
-		// toolkit.createLabel(targetArea, "target");
+		toolkit.createLabel(targetArea, "target");
 		targetSection.setClient(targetArea);
 
 		// add double click listener to dragList
-		{
-			dragList.addMouseListener(new MouseListener() {
-				public void mouseDoubleClick(MouseEvent e) {
-					itemSelected = dragList.getSelection()[0];
-					
+		dragList.addMouseListener(new MouseListener() {
+			public void mouseDoubleClick(MouseEvent e) {
+				itemSelected = dragList.getSelection()[0];
+				Image image;
+				if (itemSelected.length() != 0) {
+					if (itemSelected.equalsIgnoreCase(SWITCH)) {
+						image = Activator.getImage(SWITCH);
+					} else if (itemSelected.equalsIgnoreCase(SERVER)) {
+						image = Activator.getImage(SERVER);
+					} else if (itemSelected.equalsIgnoreCase(CLIENT)) {
+						image = Activator.getImage(CLIENT);
+					} else {
+						System.out.println("Wrong source, return");
+						return;
+					}
 					Label label = new Label(targetArea, SWT.NONE);
-					
+
 					FormData data = new FormData();
-					data.top = new FormAttachment(anchorLabel, 10,
-							SWT.TOP);
-					data.left = new FormAttachment(anchorLabel, 10,
-							SWT.LEFT);
+					data.top = new FormAttachment(anchorLabel, 10, SWT.TOP);
+					data.left = new FormAttachment(anchorLabel, 10, SWT.LEFT);
 					label.setLayoutData(data);
 
-					Image image = new Image(targetArea.getDisplay(),
-							"/home/yixin/workspace/ECS240/icons/BMPIcon.jpg");
-
+					label.setText(itemSelected);
 					label.setImage(image);
 					targetArea.layout(true);
+					// define drag operation for this label and add listner
+					DragSource dgSrc = new DragSource(label, DND.DROP_MOVE);
+					dgSrc.setTransfer(new Transfer[] { TextTransfer
+							.getInstance() });
+					dgSrc.addDragListener(new MyEntityDragSrcListener());
+					label.addMouseListener(new MyMouseListener());
 				}
+			}
 
-				public void mouseDown(MouseEvent e) {
+			public void mouseDown(MouseEvent e) {
+			}
 
-				}
-
-				public void mouseUp(MouseEvent e) {
-
-				}
-			});
-		}
+			public void mouseUp(MouseEvent e) {
+			}
+		});
 
 		textTransfer = TextTransfer.getInstance();
 
-		// define drag operation for source area
+		// define drag operation in source area
 		source = new DragSource(dragList, DND.DROP_COPY);
 		source.setTransfer(new Transfer[] { textTransfer });
 
 		// define drop operation in target area
-		target = new DropTarget(targetSection, DND.DROP_COPY | DND.DROP_DEFAULT);
+		target = new DropTarget(targetSection, DND.DROP_COPY | DND.DROP_MOVE
+				| DND.DROP_DEFAULT);
 		target.setTransfer(new Transfer[] { textTransfer });
 
-		// define drag listener
+		// define drag listener for source
 		source.addDragListener(new DragSourceListener() {
 			public void dragStart(DragSourceEvent event) {
 				String selection = dragList.getSelection()[0];
 				if (selection.length() == 0) {
 					event.doit = false;
+				}
+				if (event.detail == DND.DROP_DEFAULT) {
+					event.detail = DND.DROP_COPY;
 				}
 			}
 
@@ -180,14 +205,17 @@ public class TopologyEditorView extends ViewPart {
 			}
 		});
 
-		// define drop listener
+		// define drop listener in target
 		target.addDropListener(new DropTargetListener() {
 			public void dragEnter(DropTargetEvent event) {
+				System.out.println("dragEnter" + "| detail:" + event.detail
+						+ "| operations:" + event.operations);
+
 				if (event.detail == DND.DROP_DEFAULT) {
-					// 给event.detail赋的值必须是event.operations中的一个,event.operations中
-					// 的操作都是DragSource所支持的.
 					if ((event.operations & DND.DROP_COPY) != 0) {
 						event.detail = DND.DROP_COPY;
+					} else if ((event.operations & DND.DROP_MOVE) != 0) {
+						event.detail = DND.DROP_MOVE;
 					} else {
 						event.detail = DND.DROP_NONE;
 					}
@@ -195,13 +223,15 @@ public class TopologyEditorView extends ViewPart {
 				for (int i = 0; i < event.dataTypes.length; i++) {
 					if (textTransfer.isSupportedType(event.dataTypes[i])) {
 						event.currentDataType = event.dataTypes[i];
-						// 只允许COPY
-						if (event.detail != DND.DROP_COPY) {
+						// 只允许COPY or MOVE
+						if (event.detail != DND.DROP_COPY
+								&& event.detail != DND.DROP_MOVE) {
 							event.detail = DND.DROP_NONE;
 						}
 						break;
 					}
 				}
+				System.out.println("detail:" + event.detail);
 			}
 
 			public void dragOver(DropTargetEvent event) {
@@ -210,9 +240,13 @@ public class TopologyEditorView extends ViewPart {
 
 			public void dragOperationChanged(DropTargetEvent event) {
 				if (event.detail == DND.DROP_DEFAULT) {
-					event.detail = DND.DROP_COPY;
-				} else {
-					event.detail = DND.DROP_NONE;
+					if ((event.operations & DND.DROP_COPY) != 0) {
+						event.detail = DND.DROP_COPY;
+					} else if ((event.operations & DND.DROP_MOVE) != 0) {
+						event.detail = DND.DROP_MOVE;
+					} else {
+						event.detail = DND.DROP_NONE;
+					}
 				}
 			}
 
@@ -226,12 +260,24 @@ public class TopologyEditorView extends ViewPart {
 
 			public void drop(DropTargetEvent event) {
 				if (textTransfer.isSupportedType(event.currentDataType)) {
-					if (((String) event.data).length() != 0) {
+					String string = (String) (event.data);
+					Image image;
+					if (string.length() != 0) {
+						if (string.equalsIgnoreCase(SWITCH)) {
+							image = Activator.getImage(SWITCH);
+						} else if (string.equalsIgnoreCase(SERVER)) {
+							image = Activator.getImage(SERVER);
+						} else if (string.equalsIgnoreCase(CLIENT)) {
+							image = Activator.getImage(CLIENT);
+						} else {
+							System.out.println("Wrong source, return");
+							return;
+						}
+						// TODO: associate label to network entity instance
 						Label label = new Label(targetArea, SWT.NONE);
 
 						Point pt = display.map(null, targetArea, event.x,
 								event.y);
-						
 						FormData data = new FormData();
 						data.top = new FormAttachment(anchorLabel, pt.y,
 								SWT.TOP);
@@ -239,16 +285,57 @@ public class TopologyEditorView extends ViewPart {
 								SWT.LEFT);
 						label.setLayoutData(data);
 
-						Image image = new Image(targetArea.getDisplay(),
-								"/home/yixin/workspace/ECS240/icons/BMPIcon.jpg");
-
+						label.setText(string);
 						label.setImage(image);
 						targetArea.layout(true);
+
+						// define drag operation for this label and add listner
+						DragSource dgSrc = new DragSource(label, DND.DROP_MOVE);
+						dgSrc.setTransfer(new Transfer[] { TextTransfer
+								.getInstance() });
+						dgSrc.addDragListener(new MyEntityDragSrcListener());
+						label.addMouseListener(new MyMouseListener());
 					}
 				}
 			}
-
 		});
+	}
+
+	// define drag listener for target are source
+	class MyEntityDragSrcListener implements DragSourceListener {
+		MyEntityDragSrcListener() {
+		}
+
+		public void dragStart(DragSourceEvent event) {
+			if (event.detail == DND.DROP_DEFAULT
+					|| event.detail == DND.DROP_NONE) {
+				event.detail = DND.DROP_MOVE;
+			}
+		}
+
+		public void dragSetData(DragSourceEvent event) {
+			if (TextTransfer.getInstance().isSupportedType(event.dataType)) {
+				if (widget.getClass().getName()
+						.equalsIgnoreCase("org.eclipse.swt.widgets.Label"))
+					event.data = ((Label) widget).getText();
+			}
+		}
+
+		public void dragFinished(DragSourceEvent event) {
+			widget.dispose();
+		}
+	}
+
+	class MyMouseListener implements MouseListener {
+		public void mouseDoubleClick(MouseEvent e) {
+		}
+
+		public void mouseDown(MouseEvent e) {
+			widget = e.widget;
+		}
+
+		public void mouseUp(MouseEvent e) {
+		}
 	}
 
 	/**
