eb9909713dad83b3f1b9bf5169352bd5117b9963
Yixin Zhao <zyxivy@gmail.com>
2014-05-23 16:11:10 -0700

    modified:   SourceCode/src/ecs240/views/AttributeEditingDialog.java 	modified:   SourceCode/src/ecs240/views/TopologyEditorView.java

--- SourceCode/src/ecs240/views/AttributeEditingDialog.java
+++ SourceCode/src/ecs240/views/AttributeEditingDialog.java
@@ -2,24 +2,14 @@ package ecs240.views;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 
 public class AttributeEditingDialog extends MessageDialog {
 	private String title;
 
-	public AttributeEditingDialog(Shell parentShell, String dialogTitle,
-			Image dialogTitleImage, String dialogMessage, int dialogImageType,
-			String[] dialogButtonLabels, int defaultIndex) {
-		super(parentShell, dialogTitle, dialogTitleImage, dialogMessage,
-				dialogImageType, dialogButtonLabels, defaultIndex);
-	}
-
 	public AttributeEditingDialog(String title) {
-
 		super(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
 				title, null, "Hello World", SWT.NONE, new String[] { "Cancel",
 						"Done" }, 0);
--- SourceCode/src/ecs240/views/TopologyEditorView.java
+++ SourceCode/src/ecs240/views/TopologyEditorView.java
@@ -2,7 +2,6 @@ package ecs240.views;
 
 import java.util.ArrayList;
 
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.*;
 import org.eclipse.ui.forms.widgets.Form;
 import org.eclipse.ui.forms.widgets.FormToolkit;
@@ -17,14 +16,12 @@ import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.swt.dnd.*;
 import org.eclipse.swt.events.MouseEvent;
